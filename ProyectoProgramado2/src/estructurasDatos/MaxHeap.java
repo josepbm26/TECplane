80c1a3e8f0cea9ed6efc76c0711c3f755b9ac490
josepbm26 <josepbm26@gmail.com>
2019-10-19 00:10:50 -0600

    Revert "Creacion de interfaz de Heaps, y clases de Max Heap y Min Heap"
    This reverts commit a32078d81d9c775c82058ce3bd921bf1c473bff0.

--- ProyectoProgramado2/src/estructurasDatos/MaxHeap.java
+++ ProyectoProgramado2/src/estructurasDatos/MaxHeap.java
@@ -1,20 +1,5 @@
 package estructurasDatos;
 
+public class MaxHeap {
 
-public class MaxHeap implements Heaps{
-
-	private int[] Heap; 
-    private int size; 
-    private int maxsize; 
-	
-	
-    public MaxHeap(int maxsize){ 
-        this.maxsize = maxsize; 
-        this.size = 0; 
-        Heap = new int[this.maxsize + 1]; 
-        Heap[0] = Integer.MAX_VALUE; 
-    } 
-	    
-   
-	
 }
